1ad5061811c31dc3e2d9dcfc29adf29bfb9fa1ba
Jared Talbot <jared.talbot-2@selu.edu>
2025-04-05 11:53:00 -0500

    ye ole

--- locklets/ye_ole_locklet.gd
+++ locklets/ye_ole_locklet.gd
@@ -1,6 +1,8 @@
 class_name YeOleLocklet extends BaseLocklet
 
-@onready var randompos = choose_position()
+var randompos = Vector2.ZERO:
+	set(value):
+		randompos = value
 const all_locklets = [
 	preload("res://locklets/standard_locklet.tscn"),
 	preload("res://locklets/red_locklet.tscn"),
@@ -9,21 +11,25 @@ const all_locklets = [
 	preload("res://locklets/green_locklet.tscn")
 ] 
 
+func _ready():
+	super()
+
 func choose_position():
-	randompos = global_position + Vector2(randf_range(-200, 200), randf_range(-200, 200))
+	randompos = global_position + Vector2(randf_range(-500, 500), randf_range(-500, 500))
 
 func calculate_knockback(damage, direction: Vector2):
 	pass
 
 func calculate_movement(delta, pf):
 	super(delta, pf)
-	if navigation_agent.is_navigation_finished:
+	if navigation_agent.is_navigation_finished():
 		choose_position()
 
 func set_target_position():
 	navigation_agent.target_position = randompos
 	
-func spawn_locklets():
-	all_locklets.shuffle()
-	var locklet = all_locklets[0].instantiate()
+func spawn_locklet():
+	var locklet = all_locklets.pick_random().instantiate()
+	locklet.global_position = global_position
 	get_parent().add_child(locklet)
+	choose_position()
