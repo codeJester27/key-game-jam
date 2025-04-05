0dc3b0f6fedb1c592c855c68985f7205167fcec7
Jared Talbot <jared.talbot-2@selu.edu>
2025-04-04 20:39:01 -0500

    protag and stuff

--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
 class_name Player
 extends CharacterBody2D
 
+const HALF_PI = PI/2.0
+
 @export var SPEED = 250.0
 @onready var screen_size = get_viewport_rect().size
 @onready var key_holder = %KeyHolder
 @onready var key_holder_pivot = $KeyHolderPivot
-
+@onready var sprite = $AnimatedSprite2D
 
 var is_attacking: bool = false
 var thrust_distance: float = 60.0
@@ -19,15 +21,29 @@ func _physics_process(delta):
 		input_direction = input_direction.normalized()
 	
 	var key = get_held_key()
-	var swinging = key.is_swinging
+	var swinging = key.is_swinging if key else false
 	
 	key_holder.visible = swinging
 	
 	velocity = input_direction * (SPEED * (0.7 if key and swinging else 1.0))
-	move_and_slide()
 	
+	if velocity == Vector2.ZERO:
+		sprite.play("idle")
+	else:
+		sprite.play("walk")
+		sprite.speed_scale = velocity.length() / SPEED
+		
 	if Input.is_action_just_pressed("attack") and not swinging:
 		perform_attack(key)
+
+	if swinging:
+		var swing_angle = key_holder_pivot.global_rotation
+		sprite.flip_h = swing_angle > -HALF_PI and swing_angle < HALF_PI
+	elif velocity.x != 0:
+		sprite.flip_h = velocity.x > 0
+		
+	move_and_slide()
+
 
 func get_held_key() -> Key:
 	for child in key_holder.get_children():
@@ -41,5 +57,5 @@ func equip(key: Key):
 func perform_attack(key: Key):
 	var swing_angle = (get_global_mouse_position() - global_position).angle()
 	key_holder_pivot.global_rotation = swing_angle
-	key.swing()
-	
+	if key:
+		key.swing()
--- stab.gd
+++ stab.gd
@@ -12,4 +12,3 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Damageable"):
 		var hit_pos = $Area2D.global_position
 		body.take_damage(self, hit_pos)
-		print("hit")
