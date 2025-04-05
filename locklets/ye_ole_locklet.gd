ed58c6e9058f95a5e48a437591b71afcfe4f9d67
Jonathan Freeman <jonathan.freeman@selu.edu>
2025-04-05 11:37:19 -0500

    more lock

--- locklets/ye_ole_locklet.gd
+++ locklets/ye_ole_locklet.gd
@@ -1,6 +1,13 @@
 class_name YeOleLocklet extends BaseLocklet
 
 @onready var randompos = choose_position()
+const all_locklets = [
+	preload("res://locklets/standard_locklet.tscn"),
+	preload("res://locklets/red_locklet.tscn"),
+	preload("res://locklets/pink_locklet.tscn"),
+	preload("res://locklets/orange_locklet.tscn"),
+	preload("res://locklets/green_locklet.tscn")
+] 
 
 func choose_position():
 	randompos = global_position + Vector2(randf_range(-200, 200), randf_range(-200, 200))
@@ -16,3 +23,7 @@ func calculate_movement(delta, pf):
 func set_target_position():
 	navigation_agent.target_position = randompos
 	
+func spawn_locklets():
+	all_locklets.shuffle()
+	var locklet = all_locklets[0].instantiate()
+	get_parent().add_child(locklet)
