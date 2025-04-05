c8cea546d0fccf0b83dfa49b0aeddc831365cb81
chschlich <cohen.schlicher@selu.edu>
2025-04-05 10:44:31 -0500

    piano

---
+++ music_note.gd
@@ -0,0 +1,20 @@
+class_name Note extends Area2D 
+@export var speed := 400
+@export var damage := 10
+@export var max_lifetime := 2.0
+var direction := Vector2.RIGHT
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func set_direction(new_direction: Vector2):
+	direction = new_direction
+	rotation = direction.angle()
+
+func _physics_process(delta):
+	position += direction * speed * delta
+
+func _on_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	queue_free()
